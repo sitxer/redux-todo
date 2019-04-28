8ce08f3c5d36046a993217c131cefd5fce5d723f
Eduard Grabina <eduardgrabina@gmail.com>
2019-04-28 22:52:54 +0300

    Добавляет созранение в Localstorage

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import { createStore } from "redux";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
-import "./index.scss"
+import "./index.scss";
 
-import rootReducer from "./reducers";
+import configureStore from "./config/configureStore";
 import App from "./containers/App";
 
-const store = createStore(rootReducer);
+const { persistor, store } = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <CssBaseline />
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <CssBaseline />
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
